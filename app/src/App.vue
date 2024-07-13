79a097d6756d6f898532f584c8073e5e045fe8c7
vedem1192 <veronique.demers@polymtl.ca>
2024-07-13 14:57:53 -0400

    moving around some file + initial renaming

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,29 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="wrapper">
+        <HelloWorld msg="Hi!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/resume">Resume</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
 </template>
 
 <style scoped>
---
+++ app/src/views/ResumeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="resume">
+    <h1>This is a resume page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .resume {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
